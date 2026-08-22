6b2776616f8756834ade191fdb392ae708d9fc0f
Xevi Fernandez <informatica@imnasa.com>
2026-08-22 09:37:03 +0200

    Aprovechar todo el ancho de pantalla en horizontal (quitar límite de ancho máximo)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pirineos2026-v17';
+const CACHE = 'pirineos2026-v18';
 const ASSETS = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png', './icons/icon-180.png'];
 
 self.addEventListener('install', e => {
